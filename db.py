8421363bc6fcc5daf5ae9ae4e7fd8ee4d1e3ae55
junxing.yang <junxing.yang@nio.com>
2019-05-05 12:21:04 +0800

    adjust query fetch limit to 500

--- db.py
+++ db.py
@@ -57,7 +57,7 @@ class DBEngine(QThread):
         self.cursor = self.connection.cursor()
         self.cursor.execute(stmt)
         self.report_status()
-        results = self.cursor.fetchmany(100)
+        results = self.cursor.fetchmany(500)
         columns = self.cursor.description
         self.cursor.close()
         return results, columns
